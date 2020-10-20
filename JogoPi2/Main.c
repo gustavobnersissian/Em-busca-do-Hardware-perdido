2cd65bcfb31b2edb99b470de0e8c693a8ed8e9dd
Henrique <henriquearnaud@live.com>
2020-10-19 23:25:06 -0300

    commit em outra branch apenas com movimentacao

--- JogoPi2/Main.c
+++ JogoPi2/Main.c
@@ -1,56 +1,221 @@
-#include <allegro5/allegro5.h>
-#include <allegro5/allegro_font.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
 
-int menu(int jogando, ALLEGRO_EVENT evento) {
-    if (evento.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP) {
-        if (evento.mouse.x >= 282 &&
-            evento.mouse.x <= 353 && evento.mouse.y <= 383 &&
-            evento.mouse.y >= 359) {
-            return NULL;
-        }
-    }
+#define W 640
+#define H 480
+#define FPS 60.0
+#define FrameFps 20.0
+
+//Criação de uma struct que vai receber todos as imagens de sprite do Main Character
+struct objeto
+{
+	ALLEGRO_BITMAP* imagem;
+	float x;
+	float y;
+	int largura;
+	int altura;
+};
+
+typedef struct objeto Objeto;
+
+Objeto* personagem, * sprite_parado;
+
+//Criação de ponteiros
+ALLEGRO_DISPLAY* janela = NULL;
+ALLEGRO_EVENT_QUEUE* fila_eventos = NULL;
+ALLEGRO_BITMAP* frame = NULL;
+ALLEGRO_BITMAP* frame2 = NULL;
+ALLEGRO_TIMER* timer = NULL;
+ALLEGRO_TIMER* frametimer = NULL;
+
+bool jogando = true;
+int pressionadox = 0;
+int i = 0;
+int aux = 0;
+int j = 0;
+
+//Inicializa a biblioteca allegro com seus respectivos addons
+bool inicializar() {
+
+	al_init();
+
+	al_init_image_addon();
+	al_init_primitives_addon();
+	al_install_keyboard();
+	al_install_mouse();
+
+	timer = al_create_timer(1.0 / FPS);
+	frametimer = al_create_timer(1.0 / FrameFps);
+
+	janela = al_create_display(640, 480);
+	fila_eventos = al_create_event_queue();
+	al_clear_to_color(al_map_rgb(0, 0, 0));
+	al_set_window_title(janela, "Em busca do Hardware Perdido");
+
+	al_set_system_mouse_cursor(janela, ALLEGRO_SYSTEM_MOUSE_CURSOR_DEFAULT);
+	al_register_event_source(fila_eventos, al_get_keyboard_event_source());
+	al_register_event_source(fila_eventos, al_get_display_event_source(janela));
+	al_register_event_source(fila_eventos, al_get_timer_event_source(timer));
+	al_register_event_source(fila_eventos, al_get_timer_event_source(frametimer));
+
+
+	return true;
 }
 
-int main()
-{
-    al_init();
-    al_install_keyboard();
-    al_install_mouse();
+//Destrói os ponteiros e libera a memória
+void finalizar() {
 
-    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
-    ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
-    ALLEGRO_DISPLAY* disp = al_create_display(640, 480);
-    ALLEGRO_FONT* font = al_create_builtin_font();
+	al_destroy_display(janela);
+	al_destroy_event_queue(fila_eventos);
+	al_destroy_bitmap(frame);
+	al_destroy_bitmap(frame2);
 
-    al_register_event_source(queue, al_get_keyboard_event_source());
-    al_register_event_source(queue, al_get_mouse_event_source());
-    al_register_event_source(queue, al_get_display_event_source(disp));
-    al_register_event_source(queue, al_get_timer_event_source(timer));
+	/*al_destroy_bitmap(personagem->imagem);
+	al_destroy_bitmap(sprite_parado->imagem);
+	al_destroy_bitmap(sprite_pulando->imagem);
+	al_destroy_bitmap(frame2);
+	al_destroy_bitmap(frame3);
+	al_destroy_timer(timer);
+	free(personagem);
+	free(sprite_parado);
+	free(sprite_pulando);*/
+}
 
-    bool redraw = true;
-    ALLEGRO_EVENT evento;
 
-    int jogando = 1;
+int main(void) {
 
-    al_start_timer(timer);
-    while (jogando)
-    {
-        al_wait_for_event(queue, &evento);
 
-        
-         if ((evento.type == ALLEGRO_EVENT_KEY_DOWN) || (evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE))
-            break;
+	if (!inicializar()) {
+		return -1;
+	}
 
-        if (al_is_event_queue_empty(queue))
-        {
-            jogando = menu(jogando, evento);
-        }
-    }
+	ALLEGRO_KEYBOARD_STATE key_state;
 
-    al_destroy_font(font);
-    al_destroy_display(disp);
-    al_destroy_timer(timer);
-    al_destroy_event_queue(queue);
+	enum Direction { UP, DOWN, LEFT, RIGHT, IDLE };
 
-    return 0;
+	personagem = (Objeto*)malloc(sizeof(Objeto));
+	personagem->imagem = al_load_bitmap("Sprites/MC_Sprite_walk.png");
+	personagem->largura = 490;
+	personagem->altura = 49;
+	personagem->y = (480 / 2) - 49;
+	personagem->x = 0;
+
+	sprite_parado = (Objeto*)malloc(sizeof(Objeto));
+	sprite_parado->imagem = al_load_bitmap("Sprites/MC_Sprite_idle.png");
+	sprite_parado->x = personagem->x;
+	sprite_parado->largura = 440;
+	sprite_parado->altura = 49;
+	sprite_parado->y = personagem->y;
+	/*
+	sprite_pulando = (Objeto*)malloc(sizeof(Objeto));
+	sprite_pulando->imagem = al_load_bitmap("Sprites/MC_Sprite_jump.png");
+	sprite_pulando->largura = 469;
+	sprite_pulando->altura = 49;
+	*/
+	int dir = IDLE;
+	bool draw = false, draw2 = true;
+	int pressionando = 0;
+	int velocidade = 2;
+
+	float sourceX = 0;
+
+
+	/*for (i = 0; i < 10; i++) {
+		frame[i] = al_create_sub_bitmap(personagem->imagem, (personagem->largura / 10) * i, 0, personagem->largura / 10, personagem->altura);
+	}*/
+
+	al_start_timer(timer);
+	al_start_timer(frametimer);
+
+	while (jogando)
+	{
+		ALLEGRO_EVENT evento;
+		al_wait_for_event(fila_eventos, &evento);
+
+		al_get_keyboard_state(&key_state);
+
+		if (evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+			jogando = 0;
+		}
+
+		if (evento.type == ALLEGRO_EVENT_TIMER) {
+
+			if (evento.timer.source == timer) {
+
+				pressionando = 0;
+
+				if (al_key_down(&key_state, ALLEGRO_KEY_DOWN)) {
+					personagem->y += velocidade;
+					dir = DOWN;
+				}
+				if (al_key_down(&key_state, ALLEGRO_KEY_UP)) {
+					personagem->y -= velocidade;
+					dir = UP;
+				}
+				if (al_key_down(&key_state, ALLEGRO_KEY_RIGHT)) {
+					personagem->x += velocidade;
+					dir = RIGHT;
+
+					j = 1;
+
+					pressionando = 1;
+				}
+				if (al_key_down(&key_state, ALLEGRO_KEY_LEFT)) {
+					personagem->x -= velocidade;
+					dir = LEFT;
+					j = 0;
+
+					pressionando = 1;
+				}
+			}
+
+			if (evento.timer.source == frametimer) {
+				if (pressionando == 1) {
+					pressionando = 0;
+
+					i++;
+
+					if (i > 9) {
+						i = 0;
+					}
+
+					draw = true;
+				}
+				else if (pressionando == 0) {
+
+					i++;
+
+					if (i > 10) {
+						i = 0;
+					}
+
+					draw2 = true;
+				}
+			}
+
+		}
+		if (draw) {
+			draw = false;
+
+			frame = al_create_sub_bitmap(personagem->imagem, (personagem->largura / 10) * i, 0, personagem->largura / 10, personagem->altura);
+			al_clear_to_color(al_map_rgb(0, 0, 0));
+			al_draw_bitmap(frame, personagem->x, personagem->y, j);
+			al_flip_display();
+
+		}
+		else if (draw2) {
+			draw2 = false;
+
+			frame2 = al_create_sub_bitmap(sprite_parado->imagem, (sprite_parado->largura / 11) * i, 0, sprite_parado->largura / 11, sprite_parado->altura);
+			al_clear_to_color(al_map_rgb(0, 0, 0));
+			al_draw_bitmap(frame2, personagem->x, personagem->y, j);
+			al_flip_display();
+		}
+	}
+	finalizar();
+
+	return 0;
 }
---
+++ JogoPi2/Origem.c
@@ -0,0 +1,125 @@
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+
+
+#define LARGURA 640
+#define ALTURA 480
+
+
+int menu(jogando, evento);
+
+ALLEGRO_TIMER* timer = NULL;
+ALLEGRO_EVENT_QUEUE* fila_eventos = NULL;
+ALLEGRO_DISPLAY* janela = NULL;
+ALLEGRO_FONT* fonte = NULL;
+// Variável representando a imagem
+ALLEGRO_BITMAP* background = NULL;
+ALLEGRO_EVENT evento;
+ALLEGRO_AUDIO_STREAM* musica = NULL;
+
+
+
+void carregaArquivos() {
+
+    // Carrega a imagem
+    background = al_load_bitmap("menu.png");
+    //carrega o stream
+   
+
+}
+
+int inicializacao() {
+
+    al_init();
+    al_install_keyboard();
+    al_install_mouse();
+    al_init_image_addon();
+    fila_eventos = al_create_event_queue();
+
+    //audio
+    al_install_audio();
+    al_init_acodec_addon();
+    al_reserve_samples(5);
+
+    return 1;
+}
+
+//disp = janela
+
+int main()
+{
+
+    inicializacao();
+
+    timer = al_create_timer(1.0 / 60);
+
+    janela = al_create_display(LARGURA, ALTURA);
+
+    //isso fica no main
+    al_register_event_source(fila_eventos, al_get_keyboard_event_source());
+    al_register_event_source(fila_eventos, al_get_mouse_event_source());
+    al_register_event_source(fila_eventos, al_get_display_event_source(janela));
+
+
+
+    carregaArquivos();
+
+    // Desenha a imagem na tela
+    al_draw_bitmap(background, 0, 0, 0);
+    musica = al_load_audio_stream("musica.ogg", 4, 1024);
+    //audio
+    al_attach_audio_stream_to_mixer(musica, al_get_default_mixer());
+    al_set_audio_stream_playmode(musica, ALLEGRO_PLAYMODE_LOOP);
+
+    // Atualiza a tela
+    al_flip_display();
+
+    bool redraw = true;
+
+
+    int jogando = 1;
+
+
+
+    while (jogando)
+    {
+        al_wait_for_event(fila_eventos, &evento);
+
+
+        if ((evento.type == ALLEGRO_EVENT_KEY_DOWN) || (evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE))
+            break;
+
+        if (al_is_event_queue_empty(fila_eventos))
+        {
+            jogando = menu(jogando, evento);
+
+        }
+
+        // Atualiza a tela
+        al_flip_display();
+
+    }
+
+    al_destroy_font(fonte);
+    al_destroy_display(janela);
+    al_destroy_event_queue(fila_eventos);
+
+    return 0;
+}
+
+
+
+int menu(int jogando, ALLEGRO_EVENT evento) {
+
+    if (evento.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP) {
+        if (evento.mouse.x >= 282 &&
+            evento.mouse.x <= 353 && evento.mouse.y <= 383 &&
+            evento.mouse.y >= 359) {
+
+            return NULL;
+        }
+    }
+}
